b226384b359984ba24956153de418a2114e752ea
EduRomoGO <eduromogo@outlook.es>
2020-02-18 19:37:23 +0000

    feat: button styles

--- src/components/uic/Button/Button.js
+++ src/components/uic/Button/Button.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './Button.css';
 
 const Button = ({onClick, name}) => {
-  return <button className='.button' onClick={() => onClick(name)}>{name}</button>
+  return <button className='button button--salmon' onClick={() => onClick(name)}>{name}</button>
 };
 
 export default Button;
